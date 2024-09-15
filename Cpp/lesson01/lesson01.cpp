cffb5658a0503b2ca5a6d1ead754bb6b3b22385b
Catmarine9990 <117294785+Catmarin9990@users.noreply.github.com>
2024-09-15 16:53:35 +0400

    complited lesson1

--- Cpp/lesson01/lesson01.cpp
+++ Cpp/lesson01/lesson01.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#define M 5
+#define M 3
 
 struct Human {
     std::string name;
@@ -13,6 +13,11 @@ struct Human {
         std::cin >> surname;
         std::cout << "Enter Age: ";
         std::cin >> age;
+    }
+
+    void show(){
+        std::cout << std::endl;
+        std::cout << name << " " << surname;
     }
 };
 
@@ -33,5 +38,20 @@ struct Player{
 };
 
 int main(){
-    
+    Player player;
+    player.initialize();
+
+    int index = 0, max = player.golsCount[0];
+
+    for(int i = 1; i < M; i++){
+        if(player.golsCount[i] > max){
+            max = player.golsCount[i];
+            index = i;
+        }
+        else if(player.golsCount[i] == max && player.human[i].age < player.human[index].age){
+            index = i;
+        }
+    }
+
+    player.human[index].show();
 }
